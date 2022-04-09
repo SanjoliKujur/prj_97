372020c107846ce6adfb87f63a66dde554cb1c07
SanjoliKujur <77973219+SanjoliKujur@users.noreply.github.com>
2022-04-09 19:58:40 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,38 @@
+guess = int(input('Guess a single digit no. : '))
+n=0
+while n>=5:
+    if guess == 1:
+        print('This is not the correct no. ....Try again')
+        n+=1
+    elif guess == 2:
+        print('This is not the correct no. ....Try again')
+        n+=1
+    elif guess == 3:
+        print('This is not the correct no. ....Try again')
+        n+=1
+    elif guess == 4:
+        print('You are very close....Try again')
+        n+=1
+    elif guess == 5:
+        print('You guessed it RIGHT....Congratulations')
+        n+=1
+    elif guess == 6:
+        print('A no. less than this one  ....Try again')
+        n+=1
+    elif guess == 7:
+        print('Very close ....Try again')
+        n+=1
+    elif guess == 8:
+        print('This is not the correct no. ....Try again')
+        n+=1
+    elif guess == 9:
+        print('This is not the correct no. ....Try again')
+        n+=1
+    else:
+        print('This is not the correct no. ....Try again')
+        n+=1
+
+
+    
+
+ 
